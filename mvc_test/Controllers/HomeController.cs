8d76cbe59087c06bf7bf75364f4bd7738f1726e8
ravisd94 <74597932+ravisd94@users.noreply.github.com>
2021-05-23 21:51:07 +0530

    Add project files.

---
+++ mvc_test/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_test.Models;
+
+namespace mvc_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Homr
+        public ActionResult Index()
+        {
+            List<Product> pr_List = new List<Product>();
+            string CS = ConfigurationManager.ConnectionStrings["ProductCon"].ConnectionString;
+            
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("select * from product_master", con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Product pr = new Product();
+                    pr.prod_image = rdr["prod_image"].ToString();
+                    pr.prd_price =  float.Parse(rdr["prd_price"].ToString());
+                    pr_List.Add(pr);
+                }
+            }
+            
+            return View(pr_List);
+        }
+        public ActionResult ProductDetails(int id)
+        {
+            Product pr = new Product();
+            string CS = ConfigurationManager.ConnectionStrings["ProductCon"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("select * from product_master where prd_id ='"+id+"'", con);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    pr.prod_image = rdr["prod_image"].ToString();
+                    pr.prd_price = float.Parse(rdr["prd_price"].ToString());
+                }
+            }
+            return View(pr);
+        }
+    }
+}
---
+++ mvc_test/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace mvc_test.Models
+{
+    [Table("product_master")]
+    public class Product
+    {
+        [Key]
+        public int prd_id { get; set; }
+        public string prd_Name { get; set; }
+        public float prd_price { get; set; }
+        public float prod_weight { get; set; }
+        public string prod_image { get; set; }
+        public int prod_category_id { get; set; }
+        public int prod_stock { get; set; }
+        public int prod_sale { get; set; }
+
+    }
+}
